5f2e4442708a0ce1942d30419d1a98fac5db767e
Tiisetso Murray <timurray@student.hive.fi>
2025-11-05 17:37:59 +0200

    wip: checkpoint.

--- src/builtins/echo_builtin.c
+++ src/builtins/echo_builtin.c
@@ -6,11 +6,27 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 11:25:58 by timurray          #+#    #+#             */
-/*   Updated: 2025/11/04 16:03:16 by timurray         ###   ########.fr       */
+/*   Updated: 2025/11/05 16:41:14 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int valid_n_flag(char *arg)
+{
+	size_t i;
+
+	if (!arg || arg[0] != '-')
+		return (0);
+	i = 1;
+	while (arg[i])
+	{
+		if (arg[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 int bi_echo(char **av, t_vec *env)
 {
@@ -20,12 +36,18 @@ int bi_echo(char **av, t_vec *env)
 	(void)env;
 	while(av[i])
 	{
-		
+		if (valid_n_flag(av[i]))
+		{
+				
+		}
+		//TODO: check for -n flag
+		//TODO: print the rest
 		i++;
 	}
 	return (0);
 }
 /* 
+TODO: if valid n flag, skip it?
 TODO: write content
 TODO: write content if "-n" is detected
  */
