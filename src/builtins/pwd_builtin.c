7d9747bf84a3fe8ae90200f7098fe8eac313a452
Tiisetso Murray <timurray@student.hive.fi>
2025-11-19 12:56:54 +0200

    fix: extra arguments pwd error.

--- src/builtins/pwd_builtin.c
+++ src/builtins/pwd_builtin.c
@@ -6,7 +6,7 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 11:26:49 by timurray          #+#    #+#             */
-/*   Updated: 2025/11/05 12:24:53 by timurray         ###   ########.fr       */
+/*   Updated: 2025/11/19 12:56:30 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,12 @@ int	bi_pwd(char **av, t_vec *env)
 {
 	char	*pwd;
 
-	(void)av;
 	(void)env;
+	if(av[0] != NULL)
+	{
+		ft_putstr_fd("pwd: too many arguments\n", 2);
+		return (EXIT_FAILURE);
+	}
 	pwd = getcwd(NULL, 0);
 	if (pwd == NULL)
 	{
