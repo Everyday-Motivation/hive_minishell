c3493981e93c888ff0ba4e3f52c2d9f20978ffd2
Tiisetso Murray <timurray@student.hive.fi>
2025-11-22 17:00:11 +0200

    refactor: normed.

--- src/builtins/cd_builtin.c
+++ src/builtins/cd_builtin.c
@@ -6,94 +6,93 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 11:27:06 by timurray          #+#    #+#             */
-/*   Updated: 2025/11/21 19:18:32 by timurray         ###   ########.fr       */
+/*   Updated: 2025/11/22 16:57:10 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	bi_cd(char **av, t_info *info)
+static char	*get_address(char **av, t_vec *env)
 {
-	char *address;
-	char *oldpwd;
-	char *newpwd;
-	char *update;
-
-
-	if(av[1])
-	{
-		ft_putendl_fd("minishell: cd: too many arguments", 2);
-		return (EXIT_FAILURE);
-	}	
-	
-	oldpwd = getcwd(NULL, 0);
-	if(!oldpwd)
-	{
-		perror("getcwd error");
-		return (EXIT_FAILURE);
-	}
+	char	*address;
 
 	if (!av[0] || ft_strcmp(av[0], "~") == 0)
 	{
-		address = get_env_value(info->env, "HOME");
-		if(!address)
+		address = get_env_value(env, "HOME");
+		if (!address)
 		{
 			ft_putendl_fd("minishell: cd: HOME not set", 2);
-			free(oldpwd);
-			return (EXIT_FAILURE);
+			return (NULL);
 		}
 	}
 	else if (ft_strcmp(av[0], "-") == 0)
 	{
-		address = get_env_value(info->env, "OLDPWD");
-		if(!address)
+		address = get_env_value(env, "OLDPWD");
+		printf("%s\n", address);
+		if (!address)
 		{
 			ft_putendl_fd("minishell: cd: OLDPWD not set", 2);
-			free(oldpwd);
-			return (EXIT_FAILURE);
+			return (NULL);
 		}
 	}
 	else
 		address = av[0];
+	return (address);
+}
 
-	if (chdir(address) == -1)
-	{
-		perror("minishell: cd: No such file or directory");
-		return (EXIT_FAILURE);
-	}
+int	update_pwd(t_info *info, char *key, char *pwd)
+{
+	char	*update;
 
-	update = ft_strjoin("OLDPWD=", oldpwd);
+	update = ft_strjoin(key, pwd);
 	if (!update)
 	{
-		free(oldpwd);
-		return(EXIT_FAILURE);
-	}
-	bi_export((char *[]){ update, NULL }, info);
-	free(update);
-	free(oldpwd);
-
-	newpwd = getcwd(NULL, 0);
-	if(!newpwd)
-	{
-		perror("new directory");
+		free(pwd);
 		return (EXIT_FAILURE);
 	}
-	update = ft_strjoin("PWD=", newpwd);
-	if (!update)
-	{
-		free(newpwd);
-		return(EXIT_FAILURE);
-	}
-	bi_export((char *[]){ update, NULL }, info);
+	bi_export((char *[]){update, NULL}, info);
 	free(update);
-	free(newpwd);
-
+	free(pwd);
 	return (EXIT_SUCCESS);
 }
 
+int	get_pwd(char **pwd)
+{
+	*pwd = NULL;
+	*pwd = getcwd(NULL, 0);
+	if (!*pwd)
+	{
+		perror("getcwd error");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
 
-/* 
+int	bi_cd(char **av, t_info *info)
+{
+	char	*address;
+	char	*oldpwd;
+	char	*newpwd;
 
-TODO: shrink
-
-*/
+	if (av[1])
+	{
+		ft_putendl_fd("minishell: cd: too many arguments", 2);
+		return (EXIT_FAILURE);
+	}
+	if (get_pwd(&oldpwd) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	address = get_address(av, info->env);
+	if (chdir(address) == -1)
+	{
+		perror("minishell: cd: No such file or directory");
+		free(oldpwd);
+		return (EXIT_FAILURE);
+	}
+	if (update_pwd(info, "OLDPWD=", oldpwd) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	if (get_pwd(&newpwd) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	if (update_pwd(info, "PWD=", newpwd) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
