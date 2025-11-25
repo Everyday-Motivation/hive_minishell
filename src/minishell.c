68cab3cbc1b998a79cb7364dcc58d87618a3d4c6
Tiisetso Murray <timurray@student.hive.fi>
2025-11-25 09:34:34 +0200

    wip: updated test

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 10:23:07 by timurray          #+#    #+#             */
-/*   Updated: 2025/11/24 16:27:06 by timurray         ###   ########.fr       */
+/*   Updated: 2025/11/25 09:13:09 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,9 +88,4 @@ int	main(int ac, char **av, char **envp)
 TEST
 echo $USER    hello $USER $gjigjow $jgrei $jtier $jgier $HOME
 echo $"weird"
-
-
-
-
-<< EOF then ctrl + d segmentation fault
 */
