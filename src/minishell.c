605eb7a739015a1eeaee38f3264de96e7fe0f47e
Tiisetso Murray <timurray@student.hive.fi>
2025-11-27 15:07:46 +0200

    chore: new error.

--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaeklee <jaeklee@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 10:23:07 by timurray          #+#    #+#             */
-/*   Updated: 2025/11/27 10:29:46 by jaeklee          ###   ########.fr       */
+/*   Updated: 2025/11/27 15:04:22 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,12 +76,7 @@ int	main(int ac, char **av, char **envp)
 /*
 TEST
 
-
-echo "$""$"
-
-
-echo $ $ $
-
+echo hello""world
 
 //Found a leak with heredoc that needs to be fixed.
 valgrind ./minishell
