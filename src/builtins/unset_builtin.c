0ca754bbc13b62ac9d6364b87065d7907fc80df6
Tiisetso Murray <timurray@student.hive.fi>
2025-12-03 12:02:52 +0200

    fix: added the inability to remove _

--- src/builtins/unset_builtin.c
+++ src/builtins/unset_builtin.c
@@ -6,35 +6,46 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 11:28:04 by timurray          #+#    #+#             */
-/*   Updated: 2025/11/22 11:59:57 by timurray         ###   ########.fr       */
+/*   Updated: 2025/12/03 12:01:43 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	process_unset(t_info *info, char *av, size_t key_len)
+{
+	size_t	j;
+	char	*line;
+
+	j = 0;
+	while (j < info->env->len)
+	{
+		line = *(char **)ft_vec_get(info->env, j);
+		if (line && (ft_strncmp(av, line, key_len) == 0)
+			&& (line[key_len] == '=' || line[key_len] == '\0'))
+		{
+			vec_remove_str(info->env, j);
+			continue ;
+		}
+		j++;
+	}
+}
+
 int	bi_unset(char **av, t_info *info)
 {
 	size_t	i;
-	size_t	j;
 	size_t	key_len;
-	char	*line;
 
 	i = 0;
 	while (av[i])
 	{
 		key_len = ft_strlen(av[i]);
-		j = 0;
-		while (j < info->env->len)
+		if (key_len == 1 && av[i][0] == '_')
 		{
-			line = *(char **)ft_vec_get(info->env, j);
-			if (line && (ft_strncmp(av[i], line, key_len) == 0)
-				&& (line[key_len] == '=' || line[key_len] == '\0'))
-			{
-				vec_remove_str(info->env, j);
-				continue ;
-			}
-			j++;
+			i++;
+			continue ;
 		}
+		process_unset(info, av[i], key_len);
 		i++;
 	}
 	return (EXIT_SUCCESS);
