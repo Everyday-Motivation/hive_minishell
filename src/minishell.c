9f159f458fd2b1fe425b4fd57022fd9ab16c9170
Tiisetso Murray <timurray@student.hive.fi>
2025-12-07 12:52:43 +0200

    refactor: modified for length and removed comments.

--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaeklee <jaeklee@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 10:23:07 by timurray          #+#    #+#             */
-/*   Updated: 2025/12/03 19:01:04 by jaeklee          ###   ########.fr       */
+/*   Updated: 2025/12/07 12:48:05 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,38 @@
 
 volatile sig_atomic_t	g_signal;
 
+static void	parse_execute(t_info *info, char *line)
+{
+	t_vec	tokens;
+	t_vec	cmds;
+
+	if (tokenizing(info, line, &tokens))
+	{
+		g_signal = 0;
+		free(line);
+		free_str_vec(&tokens);
+		return ;
+	}
+	if (parse_tokens(info, &tokens, &cmds))
+	{
+		g_signal = 0;
+		free(line);
+		ft_vec_free(&tokens);
+		free_cmd_vec(&cmds);
+		return ;
+	}
+	free(line);
+	ft_vec_free(&tokens);
+	free_str_vec(&tokens);
+	if (cmds.len != 0)
+		info->exit_code = execute(&cmds, info);
+	free_cmd_vec(&cmds);
+	arena_free(info->arena);
+}
+
 static void	shell_loop(int interactive, t_info *info)
 {
 	char	*line;
-	t_vec	tokens;
-	t_vec	cmds;
 
 	while (1)
 	{
@@ -27,28 +54,7 @@ static void	shell_loop(int interactive, t_info *info)
 			break ;
 		if (g_signal == SIGINT)
 			info->exit_code = 130;
-		if (tokenizing(info, line, &tokens))
-		{
-			g_signal = 0;
-			free(line);
-			free_str_vec(&tokens);
-			continue ;
-		}
-		if (parse_tokens(info, &tokens, &cmds))
-		{
-			g_signal = 0;
-			free(line);
-			ft_vec_free(&tokens);
-			free_cmd_vec(&cmds);
-			continue ;
-		}
-		free(line);
-		ft_vec_free(&tokens);
-		free_str_vec(&tokens);
-		if (cmds.len != 0)
-			info->exit_code = execute(&cmds, info);
-		free_cmd_vec(&cmds);
-		arena_free(info->arena);
+		parse_execute(info, line);
 	}
 }
 
@@ -61,10 +67,8 @@ int	main(int ac, char **av, char **envp)
 	(void)av;
 	if (ac != 1)
 		return (return_error(NO_BINARY));
-	init_env(&env, envp);
-	// {
-	// 	return (return_error(ENV_FAIL));
-	// }
+	if (!init_env(&env, envp))
+		return (return_error(ENV_FAIL));
 	if (!arena_init(&arena))
 		return (return_error(ARENA_FAIL));
 	info.arena = &arena;
@@ -76,22 +80,3 @@ int	main(int ac, char **av, char **envp)
 	arena_free(&arena);
 	return (EXIT_SUCCESS);
 }
-
-/*
-Norm
-minishell.h: Error!
-Error: PREPROC_CONSTANT     (line:  17, col:  17):      Preprocessor statement must only contain constant defines
-
-Parsing issue:
-echo "$JUNK $JUNK hello $HOME" /SOLVED
-
-
-Test
-Applies to both of us:
-- Probably some exit code improvements
-- Mock evals with notes about issues. 
-	env -i ./minishell
-*/
-
-
-
