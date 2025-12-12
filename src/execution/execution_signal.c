cb76a71298151a45b6145d527f660fbfe3c73fc1
Tiisetso Murray <timurray@student.hive.fi>
2025-12-12 16:47:38 +0200

    fix: removed unused code.

--- src/execution/execution_signal.c
+++ src/execution/execution_signal.c
@@ -6,23 +6,11 @@
 /*   By: timurray <timurray@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 15:48:42 by timurray          #+#    #+#             */
-/*   Updated: 2025/12/12 10:28:57 by timurray         ###   ########.fr       */
+/*   Updated: 2025/12/12 16:46:27 by timurray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	parent_sig(void)
-{
-	struct sigaction	sa;
-
-	ft_bzero(&sa, sizeof(sa));
-	sa.sa_handler = sigint_handler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	sigaction(SIGINT, &sa, NULL);
-	signal(SIGQUIT, SIG_IGN);
-}
 
 void	wait_sig(void)
 {
